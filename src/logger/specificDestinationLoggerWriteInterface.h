0c7f15b5c1dd283e58c15b991195553d662673dc
Guy Avraham <guyavrah1986@gmail.com>
2025-04-14 23:17:50 +0300

    adding more notes
    Signed-off-by: Guy Avraham <guyavrah1986@gmail.com>

--- src/logger/specificDestinationLoggerWriteInterface.h
+++ src/logger/specificDestinationLoggerWriteInterface.h
@@ -2,13 +2,23 @@
 
 #include "include/myLoggerInterface.h"
 
+/*
+This interface is to facilitate a unified "entry" point to write a log message. By implementing it
+the object that does so, registers itself to be a "legitimate output destination" witihn the logger
+"bank" of different outputs (file/console/memory...).
+*/
 class ILogMessageObserver
 {
     public:
         virtual ~ILogMessageObserver(){};
         virtual void WriteLogMessage(IN const std::string& logMsg) = 0;
 };
-   
+
+/*
+This interface "completes" the Observer mechanism with the above interface. It is to be used by any class
+that aims to be the "log orchastrator", thus enabling different output destinations to regiters themselves
+within it.
+*/
 class ILogMessageSubject
 {
     public:
